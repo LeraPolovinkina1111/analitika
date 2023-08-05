2c8a620cc87a1f23c67e274ab7ed3578dc312eeb
Valera <leo.plv@bk.ru>
2023-08-05 14:52:07 +0500

    dz3

---
+++ itogdz/Main.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class Main {
+    public static void main(String[] args)  {
+        Contact contact = View.getUserInput();
+        Model.saveContact(contact);
+    }
+}
+
+
